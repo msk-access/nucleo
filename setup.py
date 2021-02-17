fd9393ebcc73739ae6d2ae21512e920b0781f9c0
Ronak Shah <rons.shah@gmail.com>
2021-02-17 09:54:51 -0500

    Bump version: 3.0.1 → 3.0.2

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/msk-access/nucleo',
-    version='3.0.1',
+    version='3.0.2',
     zip_safe=False,
 )
