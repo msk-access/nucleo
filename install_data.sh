ed47f0815c0efb2cf070fc960ed2e265613e1eaa
buehlere <buehlere77@gmail.com>
2022-08-10 13:08:27 -0400

    Update install_data.sh
    :heavy_check_mark: initial fix attempt

--- install_data.sh
+++ install_data.sh
@@ -7,21 +7,15 @@
 fileid=1GtT8jsBGwRoQC-5wHh06r8RFkiFBuirp
 
 filename=test_nucleo.tar.gz
-foldername=test_nucleo
 
-# Skip if already have test data
-[[ -f $filename ]] && exit 0
-[[ -d $foldername ]] && exit 0
-
-curl -c ./cookie -s -k -L "https://drive.google.com/uc?export=download&id=$fileid" > /dev/null
-
-curl -k -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
+pip install gdown
+gdown https://drive.google.com/uc?id=$fileid 
 
 # Suppress linux warnings for MacOS tar.gz files
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    tar --warning=no-unknown-keyword -xzvf $filename
+    tar --warning=no-unknown-keyword -xzvf $filename 
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    tar -xzvf $filename
+    tar -xzvf $filename 
 fi
 
 rm $filename
