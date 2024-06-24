c91d8aad9e8cd0bd76fa7c86aedcd002168fa460
Ronak Shah <shahr2@mskcc.org>
2024-06-24 11:52:48 -0400

    Update install_data.sh

--- install_data.sh
+++ install_data.sh
@@ -7,13 +7,13 @@
 fileid=1GtT8jsBGwRoQC-5wHh06r8RFkiFBuirp
 filename=test_nucleo.tar.gz
 
-gdown https://drive.google.com/uc?id=$fileid 
+gdown https://drive.google.com/uc?id=$fileid
 
 # Suppress linux warnings for MacOS tar.gz files
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
-    tar --warning=no-unknown-keyword -xzvf $filename 
+    tar --warning=no-unknown-keyword -xzvf $filename
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    tar -xzvf $filename 
+    tar -xzvf $filename
 fi
 
 rm $filename
