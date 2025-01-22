f6a17354e24ea1afc0a4b26b89f7b43fe89e21b3
Ronak Shah <rons.shah@gmail.com>
2025-01-22 16:36:13 -0500

    Version Update

--- setup.py
+++ setup.py
@@ -36,6 +36,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/msk-access/nucleo',
-    version='3.1.0',
+    version='3.1.1',
     zip_safe=False,
 )
